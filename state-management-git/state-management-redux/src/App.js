fc0a5be26bdbc14d4372436681644531011ce21e
Amit Singh <62277948+singhxamit21@users.noreply.github.com>
2021-10-30 10:59:51 +0530

    initial commit

---
+++ state-management-git/state-management-redux/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import {incNumber,decNumber} from "./actions/index"
+
+
+function App() {
+  const myState = useSelector((state)=>state.changeTheNumber)
+  const dispatch = useDispatch()
+  return (
+    <div className="App">
+      <div className="main-div">
+        <div class="container">
+  
+         <h1>Increment/Decrement counter</h1>
+          <h4>using React and Redux</h4>
+         <div class="quantity">
+             <a class="quantity__minus" title="Decrement" onClick={() => dispatch(decNumber())}><span>-</span></a>
+              <input name="quantity" type="text" class="quantity__input" value={myState} />
+            <a class="quantity__plus" title="Increment" onClick={() => dispatch(incNumber())}><span>+</span></a>
+          </div>
+  
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
